2d7693b6756a4f14832bf6dcad4cb96c0b12a458
Matt Zhao <matt.zhao2010@gmail.com>
2024-05-14 21:00:26 -0300

    some scripts

---
+++ keyEvent.js
@@ -0,0 +1,7 @@
+const tx = document.querySelector('tx');
+const item = document.querySelector('div.item')
+const total = document.querySelector('div.total')
+const info = document.querySelector('div.info')
+
+
+
